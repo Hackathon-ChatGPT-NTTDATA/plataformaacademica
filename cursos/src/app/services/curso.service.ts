c650ff77d676550469bb317c50057b6606c78483
carlo delgado <cdmarallano23@gmail.com>
2023-05-01 02:38:00 -0500

    chore[8] agregando en el services crud alumno, curso

--- cursos/src/app/services/curso.service.ts
+++ cursos/src/app/services/curso.service.ts
@@ -1,9 +1,40 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Curso } from '../models/curso';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CursoService {
 
-  constructor() { }
+  private baseEndpoint = 'localhost:8090/api/alumnos';
+  private cabeceras: HttpHeaders = new HttpHeaders({'ContentType': 'application/json'});
+
+  constructor(private http: HttpClient) { }
+
+  public  listar (): Observable<Curso[]>{
+   return this.http.get<Curso[]>(this.baseEndpoint);
+  }
+
+  public listarpaguinas(page: string, size: string): Observable<any>{
+    const params =new HttpParams()
+    .set('page', page)
+    .set('size', size);
+return this.http.get<any>(`${this.baseEndpoint}/pagina`,{params: params});
+  }
+
+  public ver (id: number): Observable<Curso>{
+   return this.http.get<Curso>(`${this.baseEndpoint}/${id}`);
+  }
+  public  crear (curso:Curso): Observable<Curso>{
+    return this.http.post<Curso>(this.baseEndpoint,curso, {headers: this.cabeceras});
+  }
+ public editar (curso:Curso): Observable<Curso>{
+  return this.http.put<Curso>(`${this.baseEndpoint}/${curso.id}`,curso,{headers: this.cabeceras});
+ }
+public eliminar (id: number): Observable<void>{
+  return this.http.delete<void>(`${this.baseEndpoint}/${id}`);
+}
 }
